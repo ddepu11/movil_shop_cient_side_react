71b8cca71c4986c46446e6fce24dcc278c08cf36
Deepanshu Tiwari <ddepu11@gmail.com>
2021-04-12 11:06:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "./components";
+import styled from "styled-components";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+const Wrapper = styled.div`
+  
+`;
+
+export default App;
