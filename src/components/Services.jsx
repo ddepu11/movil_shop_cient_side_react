c57c37324678bc9866168976fd8f573bc4318404
Deepanshu Tiwari <ddepu11@gmail.com>
2021-04-12 19:50:13 +0530

    icons

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import { AiOutlineShoppingCart, AiFillSetting } from "react-icons/ai";
+import { FiRefreshCcw } from "react-icons/fi";
 
 const Services = () => {
-  return <Wrapper>Services</Wrapper>;
+  return <Wrapper></Wrapper>;
 };
 
 const Wrapper = styled.section``;
