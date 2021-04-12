bf80d5771cd2e67ad3b54f03d1535a2eba9eb4a6
Deepanshu Tiwari <ddepu11@gmail.com>
2021-04-12 13:02:15 +0530

    logo added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Navbar } from "./components";
 import styled from "styled-components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Wrapper>
+      <Router>
+        <Navbar />
+      </Router>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
-  
+  max-width: 1200px;
+  margin: 0 auto;
 `;
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import logo from "./assests/logo.svg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Wrapper>
+      <div className="nav_top flex">
+        <div className="contact">
+          <ul className="flex">
+            <li>
+              <span>8268116588</span>
+            </li>
+            <li>
+              <span>movilshop@gmail.com</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="links">
+          <ul className="flex">
+            <li>
+              <Link to="/account">Account</Link>
+            </li>
+            <li>
+              <Link to="/log-in">LogIn</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="nav_bottom">
+        <img src={logo} alt="" />
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.nav`
+  max-width: 960px;
+  margin: 0 auto;
+  .nav_top {
+    justify-content: space-between;
+    padding: 30px 5px;
+
+    .contact {
+      width: 30%;
+    }
+    .contact ul,
+    .links ul {
+      justify-content: space-between;
+    }
+
+    .links {
+      width: 15%;
+    }
+  }
+`;
 
 export default Navbar;
