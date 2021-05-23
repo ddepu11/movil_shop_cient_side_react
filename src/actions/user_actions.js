1c52da24f796ee30c5cbc00220b1cced3ac2f89c
Deepanshu Tiwari <ddepu11@gmail.com>
2021-05-23 16:12:28 +0530

    installed axios

--- src/actions/user_actions.js
+++ src/actions/user_actions.js
@@ -8,9 +8,10 @@ const userAuthLoggedIn = (user) => (dispatch) => {
   dispatch({ type: SET_USER_LOGGED_IN, payload: user });
 };
 
-const customUserLogin = (email, password) => (dispatch) => {
+const customUserLogin = (email, password) => async (dispatch) => {
   dispatch({ type: USER_LOG_IN_BEGIN });
   try {
+    
   } catch (err) {
     console.log(err);
     dispatch({ type: USER_LOG_IN_ERROR, payload: err.message });
