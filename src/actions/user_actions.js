b0326bfdf221d16b25fd44ca552505ceb05e4c7c
Deepanshu Tiwari <ddepu11@gmail.com>
2021-05-25 10:02:40 +0530

    SignUp function now returns something

--- src/actions/user_actions.js
+++ src/actions/user_actions.js
@@ -2,6 +2,8 @@ import {
   SET_USER_LOGGED_IN,
   USER_LOG_IN_BEGIN,
   USER_LOG_IN_ERROR,
+  USER_SIGN_UP_BEGIN,
+  USER_SIGN_UP_ERROR,
 } from "./constant";
 import * as user from "../api/user_api.js";
 
@@ -26,14 +28,15 @@ const customUserLogin = (email, password) => async (dispatch) => {
 };
 
 const signUpUser = (userCredentials) => async (dispatch) => {
-  dispatch({ type: USER_LOG_IN_BEGIN });
+  // dispatch({ type: USER_SIGN_UP_BEGIN });
 
   try {
-    const { data } = await user.signUp({ ...userCredentials });
-    console.log(data);
+    const res = await user.signUp(userCredentials);
+    console.log(res);
   } catch (error) {
     console.log(error);
-    dispatch({ type: USER_LOG_IN_ERROR });
+
+    // dispatch({ type: USER_SIGN_UP_ERROR });
   }
 };
 
--- src/api/user_api.js
+++ src/api/user_api.js
@@ -3,22 +3,7 @@ import axios from "axios";
 const logIn = (email, password) =>
   axios.post("http://localhost:5000/user/login", { email, password });
 
-const signUp = ({
-  firstName,
-  lastName,
-  phoneNumber,
-  email,
-  password,
-  confirmPassword,
-}) => {
-  axios.post("http://localhost:5000/sign-up", {
-    firstName,
-    lastName,
-    phoneNumber,
-    email,
-    password,
-    confirmPassword,
-  });
-};
-
+const signUp = (userCredentials) =>
+  axios.post("http://localhost:5000/user/sign-up", { ...userCredentials });
+  
 export { logIn, signUp };
