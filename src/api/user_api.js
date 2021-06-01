3a2049b0581ed10c056d157d3402b1207328dcde
Deepanshu Tiwari <ddepu11@gmail.com>
2021-06-01 10:07:09 +0530

    proxy added

--- src/api/user_api.js
+++ src/api/user_api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
+
 const logIn = (email, password) =>
-  axios.post("http://localhost:5000/user/login", { email, password });
+  axios.post("/user/login", { email, password });
 
 const signUp = (userCredentials) =>
-  axios.post("http://localhost:5000/user/sign-up", { ...userCredentials });
+  axios.post("/user/sign-up", { ...userCredentials });
 
 export { logIn, signUp };
