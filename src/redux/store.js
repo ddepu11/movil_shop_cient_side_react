3131a537a5f19e86b7c7dc92df4eb2ace33e5842
Deepanshu Tiwari <ddepu11@gmail.com>
2021-08-08 18:35:06 +0530

    refactor: dont use redux dev tools in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from '../reducers';
 
+const NODE_ENV = process.env.REACT_APP_NODE_ENV;
+
+const composeEnhancer =
+  NODE_ENV === 'development' ? composeWithDevTools : compose;
+
 const createAStore = () =>
-  createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+  createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
 
 export default createAStore;
