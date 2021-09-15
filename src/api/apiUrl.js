0266b4ce8fb2d318f2b81913bd0de3cd480c0837
Deepanshu Tiwari <ddepu11@gmail.com>
2021-09-15 14:33:13 +0530

    chore: targeting prodction api

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -1,4 +1,4 @@
-// const apiUrl = 'https://movil-shop.herokuapp.com';
-const apiUrl = 'http://localhost:5000';
+const apiUrl = 'https://movil-shop.herokuapp.com';
+// const apiUrl = 'http://localhost:5000';
 
 export default apiUrl;
