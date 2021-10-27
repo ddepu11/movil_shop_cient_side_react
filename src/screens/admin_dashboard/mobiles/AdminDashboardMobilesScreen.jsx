49e5b72c094ded81e75bd35f44bce3382c7b15c1
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-27 14:43:35 +0530

    refactor: admin delete mobile and it's pics from firebase

--- src/screens/admin_dashboard/mobiles/AdminDashboardMobilesScreen.jsx
+++ src/screens/admin_dashboard/mobiles/AdminDashboardMobilesScreen.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { ref, deleteObject } from 'firebase/storage';
+import { storageInstance } from '../../../config/firebase';
 import { deleteMobile, listMobiles } from '../../../actions/adminActions';
 import CircleLoader from '../../../components/CircleLoader';
 import Mobile from '../../../components/Mobile';
+import {
+  ADMIN_DELETE_MOBILE_BEGIN,
+  ADMIN_DELETE_MOBILE_ERROR,
+} from '../../../constants/adminConstants';
+import { sendNotification } from '../../../actions/notificationActions';
 
 const AdminDashboardMobilesScreen = () => {
   const { mobiles, adminLoading } = useSelector((state) => state.admin);
@@ -13,8 +20,38 @@ const AdminDashboardMobilesScreen = () => {
     mobiles.length === 0 && dispatch(listMobiles());
   }, [dispatch, mobiles]);
 
+  const deleteMobilePic = (mobileId) => {
+    const { pictures, sellerInfo } = mobiles.filter(
+      (item) => item._id === mobileId
+    )[0];
+
+    try {
+      let index = 0;
+
+      pictures.forEach(async (item) => {
+        const mobilePicRef = ref(
+          storageInstance,
+          `mobileImages/${sellerInfo.email}/${item.fileName}`
+        );
+
+        await deleteObject(mobilePicRef);
+
+        if (pictures.length - 1 === index) {
+          console.log('Delete Mobile Doc');
+          dispatch(deleteMobile(mobileId));
+        }
+
+        index += 1;
+      });
+    } catch (err) {
+      dispatch({ type: ADMIN_DELETE_MOBILE_ERROR });
+      dispatch(sendNotification(err.code, true));
+    }
+  };
+
   const handleDelete = (mobileId) => {
-    dispatch(deleteMobile(mobileId));
+    dispatch({ type: ADMIN_DELETE_MOBILE_BEGIN });
+    deleteMobilePic(mobileId);
   };
 
   if (adminLoading) {
