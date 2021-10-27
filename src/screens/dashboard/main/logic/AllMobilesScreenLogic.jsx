8d0df47d02d2b9851d284eff5a292a9b336b2e62
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-27 13:22:44 +0530

    feat: delete mobile images from firebase

--- src/screens/dashboard/main/logic/AllMobilesScreenLogic.jsx
+++ src/screens/dashboard/main/logic/AllMobilesScreenLogic.jsx
@@ -1,6 +1,14 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { ref, deleteObject } from 'firebase/storage';
+import { storageInstance } from '../../../../config/firebase';
+
+import { sendNotification } from '../../../../actions/notificationActions';
 import { listMobiles, removeMobile } from '../../../../actions/sellerActions';
+import {
+  SELLER_MOBILE_DELETE_BEGIN,
+  SELLER_MOBILE_DELETE_ERROR,
+} from '../../../../constants/sellerConstants';
 
 const AllMobilesScreenLogic = () => {
   const { userInfo } = useSelector((state) => state.user);
@@ -13,8 +21,36 @@ const AllMobilesScreenLogic = () => {
       dispatch(listMobiles(userInfo._id));
   }, [dispatch, userInfo, sellerMobiles]);
 
+  const deleteMobilePic = (id) => {
+    const { pictures } = sellerMobiles.filter((item) => item._id === id)[0];
+
+    try {
+      let index = 0;
+
+      pictures.forEach(async (item) => {
+        const mobilePicRef = ref(
+          storageInstance,
+          `mobileImages/${userInfo.email}/${item.fileName}`
+        );
+
+        await deleteObject(mobilePicRef);
+
+        if (pictures.length - 1 === index) {
+          dispatch(removeMobile(id));
+        }
+
+        index += 1;
+      });
+    } catch (err) {
+      dispatch({ type: SELLER_MOBILE_DELETE_ERROR });
+
+      dispatch(sendNotification(err.code, true));
+    }
+  };
+
   const handleDeleteMobile = (id) => {
-    dispatch(removeMobile(id));
+    dispatch({ type: SELLER_MOBILE_DELETE_BEGIN });
+    deleteMobilePic(id);
   };
 
   return { handleDeleteMobile, sellerLoading, userInfo, sellerMobiles };
