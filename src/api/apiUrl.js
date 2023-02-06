d4ee98aeb1a9d63a4937c4a314b4457259c250a8
deepanshu tiwari <deepanshut@geekyants.com>
2023-02-07 00:44:41 +0530

    refactor: change url

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://movil-shop.up.railway.app';
+const apiUrl = 'https://movilshopserverside-production.up.railway.app';
 // const apiUrl = 'http://localhost:5000';
 
 export default apiUrl;
