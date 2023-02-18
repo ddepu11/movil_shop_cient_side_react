292e3bb1e83b758187d88843a8a6769674c062e6
deepanshu tiwari <deepanshut@geekyants.com>
2023-02-18 23:22:25 +0530

    refactor:use 4000 port for local

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -1,4 +1,4 @@
 const apiUrl = 'https://movilshopserverside-production.up.railway.app';
-// const apiUrl = 'http://localhost:5000';
+// const apiUrl = 'http://localhost:4000';
 
 export default apiUrl;
